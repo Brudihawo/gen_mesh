886f09290a0741e49599208777263140c44fda9a
Hawo Höfer <hawo.hoefer98@freenet.de>
2022-01-29 15:13:44 +0100

    [logging] add warn and verbosity

--- src/logging.c
+++ src/logging.c
@@ -3,15 +3,15 @@
 
 #include "logging.h"
 
-void _log(const char* func, const char *file, int line, const char *fmt, ...) {
-#ifdef DEBUG
-  va_list args;
-  fprintf(stderr, "%s:%i (%s): ", file, line, func);
-  va_start(args, fmt);
+void _log(PrintVerbosity level, const char* func, const char *file, int line, const char *fmt, ...) {
+  if (level <= VERB_LEVEL) {
+    va_list args;
+    fprintf(stderr, "%s:%i (%s): ", file, line, func);
+    va_start(args, fmt);
 
-  vfprintf(stderr, fmt, args);
-  fprintf(stderr, "\n");
+    vfprintf(stderr, fmt, args);
+    fprintf(stderr, "\n");
 
-  va_end(args);
-#endif
+    va_end(args);
+  }
 }
--- src/logging.h
+++ src/logging.h
@@ -1,7 +1,19 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 
-#define log_msg(...) _log(__FUNCTION__, __FILE__, __LINE__, __VA_ARGS__)
-void _log(const char* func, const char *file, int line, const char *fmt, ...);
+typedef enum {
+  VERB_ERR = 0,
+  VERB_WRN,
+  VERB_DBG,
+} PrintVerbosity;
+
+// TODO: Fix Verbosity thing
+#ifndef VERB_LEVEL
+#define VERB_LEVEL VERB_DBG
+#endif
+
+#define log_msg(...) _log(VERB_DBG, __FUNCTION__, __FILE__, __LINE__, __VA_ARGS__)
+#define log_wrn(...) _log(VERB_WRN, __FUNCTION__, __FILE__, __LINE__, __VA_ARGS__)
+void _log(PrintVerbosity level, const char* func, const char *file, int line, const char *fmt, ...);
 
 #endif // LOGGING_H
